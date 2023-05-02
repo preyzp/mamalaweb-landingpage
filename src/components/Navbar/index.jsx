40bcdc8c225927e1b298365578daebcfdb1f233d
Zakharias Priya <priyazakharias@gmail.com>
2023-05-02 09:32:32 +0700

    fix latest code

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+export default function Navbar() {
+  return (
+    <div className="navbar" data-scroll-section>
+      <div>menu</div>
+
+      <div>Mamala</div>
+
+      <div>about</div>
+    </div>
+  );
+}
